e20baada0b46673096d6d8cb064d795119d7e359
hgthobbi <hemantgthobbi@gmail.com>
2024-03-14 12:53:42 -0700

    built home page, posting, stories, light/dark mode

---
+++ src/components/comments/comments.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const comments = () => {
+    const comments = [
+        {
+            id: 1,
+            name: "Driver 1",
+            userId: 1,
+            profilePic: DP,
+            desc: "picture I took of the r8 today.",
+            img: DP
+        },
+        {
+            id: 2,
+            name: "Driver 2",
+            userId: 2,
+            profilePic: story3,
+            desc: "Boost or No Boost?",
+            img: story3
+        },
+        
+    ];
+  return (
+    <div className = "comments">
+      
+    </div>
+  )
+}
+
+export default comments
